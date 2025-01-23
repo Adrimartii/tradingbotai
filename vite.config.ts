21904f438feaff6cbef4a868c0468c27a161cd5a
Adrimartii <100836631+Adrimartii@users.noreply.github.com>
2025-01-23 22:52:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {
-      BINANCE_API_KEY: JSON.stringify(process.env.BINANCE_API_KEY),
-      BINANCE_API_SECRET: JSON.stringify(process.env.BINANCE_API_SECRET)
+    'import.meta.env.VITE_BINANCE_API_KEY': JSON.stringify(process.env.VITE_BINANCE_API_KEY),
+    'import.meta.env.VITE_BINANCE_API_SECRET': JSON.stringify(process.env.VITE_BINANCE_API_SECRET),
+    'import.meta.env.VITE_OPENAI_API_KEY': JSON.stringify(process.env.VITE_OPENAI_API_KEY),
+    'import.meta.env.VITE_SERPAPI_API_KEY': JSON.stringify(process.env.VITE_SERPAPI_API_KEY)
     }
-  },
+  }
 });
